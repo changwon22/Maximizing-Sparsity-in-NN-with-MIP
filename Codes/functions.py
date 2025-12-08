35238e9078afe3471ed0de8690c90d84178f8ffa
changwon22 <22changwon@gmail.com>
2025-12-07 20:23:39 -0600

    minor edit

---
+++ Codes/functions.py
@@ -0,0 +1,11 @@
+import numpy as np
+import gurobipy as gp
+from gurobipy import GRB
+
+# First Stage: Finding feasible width K
+
+# Second Stage: For a given K, sparsify the NN weights
+
+# MINLP
+
+# MILP
